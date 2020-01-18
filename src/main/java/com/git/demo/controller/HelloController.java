5adaa495fee35c690a751629dc91fb49dd47c31e
weihz <65161496@qq.com>
2020-01-18 14:03:57 +0800

    首次提交

---
+++ src/main/java/com/git/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.git.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Object  hello(){
+        return "hello";
+    }
+
+}
